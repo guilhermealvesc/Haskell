f327ea7b2b8e9f98a323535cb6896951c123d95a
guilherme.092011 <guilherme.092011@hotmail.com>
2021-03-17 10:09:07 -0300

    recursividade

---
+++ training/recursividade.hs
@@ -0,0 +1,16 @@
+binoG :: (Int, Int) -> Int
+
+-- binoG (n, k) 
+--   | k == 0 = 1
+--   | n == k = 1
+--   | otherwise = binoG(n - 1, k) + binoG(n - 1, k - 1)
+
+-- Dúvida
+-- binoG (n, 0) = 1
+-- binoG (n, k) = if n == k then 1
+-- binoG (n, k) = binoG(n - 1, k) + binoG(n - 1, k - 1)
+
+binoG (n, 0) = 1
+binoG (n, k) = 
+  if n == k then 1
+  else binoG(n - 1, k) + binoG(n - 1, k - 1)
