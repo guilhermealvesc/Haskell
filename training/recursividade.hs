de44b7fd496e5a251dd9aa9238258ca14d826bb0
guilherme.092011 <guilherme.092011@hotmail.com>
2021-03-19 13:00:34 -0300

    fibonacci otimizado (quase tive um avc nessa porra)

--- training/recursividade.hs
+++ training/recursividade.hs
@@ -5,11 +5,6 @@ binoG :: (Int, Int) -> Int
 --   | n == k = 1
 --   | otherwise = binoG(n - 1, k) + binoG(n - 1, k - 1)
 
--- Dúvida
--- binoG (n, 0) = 1
--- binoG (n, k) = if n == k then 1
--- binoG (n, k) = binoG(n - 1, k) + binoG(n - 1, k - 1)
-
 binoG (n, 0) = 1
 binoG (n, k) = 
   if n == k then 1
